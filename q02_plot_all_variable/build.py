45444531b76e5714547a7651d4cdc568a361fb17
abkedar <anupbhande@gmail.com>
2018-09-15 13:29:23 +0000

    Done

--- q02_plot_all_variable/build.py
+++ q02_plot_all_variable/build.py
@@ -7,3 +7,22 @@ plt.switch_backend('agg')
 from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
+
+def q02_plot_all_variable(path, cols=5):
+    'write your solution here'
+    df = q01_load_data_and_add_column_names(path)
+    fig = plt.figure(figsize=(20, 15))
+    rows = ceil(float(df.shape[1]) / cols)
+    for i, column in enumerate(df.columns):
+        ax = fig.add_subplot(rows, cols, i + 1)
+        ax.set_title(column)
+        if df.dtypes[column] == np.object:
+            df[column].value_counts().plot(kind='bar', axes=ax)
+        else:
+            df[column].hist(axes=ax)
+            plt.xticks(rotation='vertical')
+    plt.subplots_adjust(hspace=0.7, wspace=0.2)
+    plt.show()
+
+
+
--- q03_encode_features/build.py
+++ q03_encode_features/build.py
@@ -1,3 +1,4 @@
+# %load q03_encode_features/build.py
 import pandas as pd
 import numpy as np
 import sys, os
@@ -6,4 +7,17 @@ from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q
 
 path = 'data/GermanData.csv'
 
+def q03_encode_features(data):
+    data_1 = q01_load_data_and_add_column_names(data)
+    data1 = data_1.copy()
+    dic = {}
+    df= data_1.select_dtypes(include=['object'])
 
+    # Initilalizing LabelEncoder()
+    le = LabelEncoder()
+
+    df_encoded = df.apply(le.fit_transform)
+    data_1.update(df_encoded)
+    return(data_1)
+
+q03_encode_features(path)
