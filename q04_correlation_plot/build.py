b8c209fc0ed648c6b7df356917ed1f6493abd811
sonik8494 <sourabh.niks1994@gmail.com>
2018-10-24 07:08:13 +0000

    Done

--- q04_correlation_plot/build.py
+++ q04_correlation_plot/build.py
@@ -1,9 +1,13 @@
+# %load q04_correlation_plot/build.py
 import matplotlib.pyplot as plt
 import seaborn as sns
 plt.switch_backend('agg')
 import sys, os
 from greyatomlib.german_credit.q03_encode_features.build import q03_encode_features
 path = 'data/GermanData.csv'
+def q04_correlation_plot(path):
+    df = q03_encode_features(path)
+    a,b = df
+    return a.corr()
 
 
-   
