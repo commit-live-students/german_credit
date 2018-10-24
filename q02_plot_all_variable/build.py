f6a92a2e5cee2b35ebc3102feb44d39532d21f0b
sonik8494 <sourabh.niks1994@gmail.com>
2018-10-24 03:24:56 +0000

    Done

--- q02_plot_all_variable/build.py
+++ q02_plot_all_variable/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_all_variable/build.py
 import pandas as pd
 import numpy as np
 import sys, os
@@ -7,3 +8,8 @@ plt.switch_backend('agg')
 from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
+def q02_plot_all_variable(path): 
+    df = q01_load_data_and_add_column_names(path)
+    return pd.plotting.scatter_matrix(df)
+
+
--- q03_encode_features/build.py
+++ q03_encode_features/build.py
@@ -1,3 +1,4 @@
+# %load q03_encode_features/build.py
 import pandas as pd
 import numpy as np
 import sys, os
@@ -5,5 +6,18 @@ from sklearn.preprocessing import LabelEncoder
 from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
+
+
+le = LabelEncoder()
+dictionary = {}
+categ = []
+def q03_encode_features(path):
+    df = q01_load_data_and_add_column_names(path)
+    for x in df:
+        if df.dtypes[x] == 'object':
+            categ.append(x)
+    for x in categ:
+        df[x] = le.fit_transform(df[x])
+    return df,dictionary
 
 
