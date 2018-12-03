aa43823b201c5b71ad20c087b2063aa3a6f8a5e4
prodigy803 <pushkaraj803@gmail.com>
2018-12-03 09:22:45 +0000

    Done

--- q02_plot_all_variable/build.py
+++ q02_plot_all_variable/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_all_variable/build.py
 import pandas as pd
 import numpy as np
 import sys, os
@@ -7,3 +8,13 @@ plt.switch_backend('agg')
 from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
+def q02_plot_all_variable(path,rand_var=5):
+    data = q01_load_data_and_add_column_names(path)
+    numeric_cols = data.select_dtypes(include=['int64'])
+    pd.plotting.scatter_matrix(numeric_cols)
+    plt.show()
+    
+
+
+
+
