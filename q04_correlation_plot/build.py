119bbba68c5c4a4afe6ad3509ef503d74057148d
prodigy803 <pushkaraj803@gmail.com>
2018-12-03 09:47:12 +0000

    Done

--- q04_correlation_plot/build.py
+++ q04_correlation_plot/build.py
@@ -1,3 +1,5 @@
+# %load q04_correlation_plot/build.py
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 plt.switch_backend('agg')
@@ -5,5 +7,12 @@ import sys, os
 from greyatomlib.german_credit.q03_encode_features.build import q03_encode_features
 path = 'data/GermanData.csv'
 
+def q04_correlation_plot(path):
+    data = q03_encode_features(path)
+    sns.pairplot(data[0])
+    plt.show()
+    
 
-   
+
+
+
