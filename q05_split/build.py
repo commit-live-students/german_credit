802b1aa07dc4129c795eb271aaeb98c98a5f4f3f
prodigy803 <pushkaraj803@gmail.com>
2018-12-03 09:53:08 +0000

    Done

--- q05_split/build.py
+++ q05_split/build.py
@@ -1,6 +1,17 @@
+# %load q05_split/build.py
 import sys, os
 from sklearn.model_selection import train_test_split
 from greyatomlib.german_credit.q03_encode_features.build import q03_encode_features
 path = 'data/GermanData.csv'
+
+def q05_split(path,test_size=0.2,random_state=9):
+    data = q03_encode_features(path)
+    X = data[0].iloc[:,:-1]
+    y = data[0].iloc[:,-1]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size,random_state=random_state)
+    
+    return X_train, X_test, y_train, y_test
+
+
 
 
