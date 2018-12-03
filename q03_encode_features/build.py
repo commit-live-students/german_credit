e7830ce7d9d11ade9fadda8e8778fe25ee70ef32
prodigy803 <pushkaraj803@gmail.com>
2018-12-03 09:31:09 +0000

    Done

--- q03_encode_features/build.py
+++ q03_encode_features/build.py
@@ -1,3 +1,4 @@
+# %load q03_encode_features/build.py
 import pandas as pd
 import numpy as np
 import sys, os
@@ -5,5 +6,24 @@ from sklearn.preprocessing import LabelEncoder
 from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
+
+def q03_encode_features(path):
+    dict1 = {}
+    data = q01_load_data_and_add_column_names(path)
+    data_copy = data.copy()
+
+    cat_features = data.select_dtypes(include=['object'])
+    cat_features_array = np.array(cat_features.columns)
+
+
+    lE = LabelEncoder()
+    for i in range(len(cat_features_array)):
+        data_copy.loc[:,cat_features_array[i]] = lE.fit_transform(data_copy.loc[:,cat_features_array[i]])
+        
+    return data_copy,dict1
+
+
+
+
 
 
