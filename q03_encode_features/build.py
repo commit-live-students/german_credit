e64b6edad7baa65dd88f406928d8e03a23551c51
sannidh <soniknitr@gmail.com>
2019-01-17 00:34:26 +0000

    Done

--- q03_encode_features/build.py
+++ q03_encode_features/build.py
@@ -1,9 +1,30 @@
+# %load q03_encode_features/build.py
 import pandas as pd
 import numpy as np
 import sys, os
 from sklearn.preprocessing import LabelEncoder
-from greyatomlib.german_credit.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
-path = 'data/GermanData.csv'
+def q01_load_data_and_add_column_names(path='data/GermanData.csv'):
+    df = pd.read_csv(path, header=None)
+    df.columns = ['account_status', 'month', 'credit_history', 'purpose', 'credit_amount', 'savings_account/bonds', 
+                                  'employment', 'installment_rate', 'personal_status/sex', 'guarantors', 'residence_since', 
+                                  'property', 'age', 'other_installment_plans', 'housing', 'number_of_existing_credits', 'job', 
+                                  'liable', 'telephone', 'foreign_worker', 'good/bad']
+    df['good/bad']= (df['good/bad']>1).astype(int)
+    return df
+
+def q03_encode_features(path='data/GermanData.csv'):
+    df = q01_load_data_and_add_column_names(path)
+    df_new = df.copy()
+    le = LabelEncoder()
+    cols = {}
+    cat_var = [a for a in range(len(df_new.dtypes)) if df_new.dtypes[a] in ['object']]
+    for column in cat_var:
+        df_new.iloc[:, column] = le.fit_transform(df_new.iloc[:, column]) 
+        cols[column] = df.columns[column]
+    return df_new, cols
+
+
+
 
 
